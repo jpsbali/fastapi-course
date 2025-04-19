e9e6fec4223255819380771bd9d0e41d7c18d3db
Jatinder Bali <jpsbali@gmail.com>
2025-04-18 22:15:01 -0400

    Removed blanks from main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 
-
 app = FastAPI()
-
 
 @app.get('/blog')
 def index(limit=10, published: bool = True, sort: Optional[str] = None):
